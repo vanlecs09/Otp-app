bc283e547ca40ecb10b1f9660844ee4224535655
van le <vanlecs09@gmail.com>
2019-04-24 16:40:30 +0700

    ~ ad side bar

--- sidebar/SideBar.js
+++ sidebar/SideBar.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { TextInput, ImageBackground, Image, StyleSheet, Text, View, Button } from 'react-native';
+import { ListView,TextInput, ImageBackground, Image, StyleSheet, Text, View, Button } from 'react-native';
+import SlideBarItem from './SideBarItem';
 
 export default class SideBar extends React.Component {
     constructor(props) {
         super(props);
-      }
-      
-      render() {
-          return (
-              <Text>
-                  this is side bar
-              </Text>
-          )
-      }
+    }
+
+    render() {
+        return (
+            <ImageBackground source={require('./res/img_sidebar_bg.png')} style={styles.background}>
+                {/* <Text>
+                    this is side bar
+               </Text> */}
+            <ListView>
+                </ListView>
+            </ImageBackground>
+        )
+    }
 }
+
+var styles = StyleSheet.create({
+
+    background: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    }
+});
---
+++ sidebar/SideBarItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { TextInput, ImageBackground, Image, StyleSheet, Text, View, Button } from 'react-native';
+
+export default class SideBarItem extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    return() {
+        return (
+            <Text>
+                this is item
+                </Text>
+            // <View>
+            //     <Image>
+            //     </Image>
+            //     <Text>
+            //     </Text>
+            //     <Text>
+
+            //     </Text>
+            // </View>
+        )
+    }
+}
